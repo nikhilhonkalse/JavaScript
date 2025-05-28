ff72dfd7fb9b1f986d9b398c8383244f6e71d0a3
Nikhil Honkalse <nikhilhonkalse720@gmail.com>
2025-05-28 23:54:26 +0530

    Basics

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountID = 32454
+let accountEmail = "nik@google.com"
+var accountPassword = "367777"
+accountCity = "Pune"
+let accountState
+
+accountEmail  = "nikh234@google.com"
+accountPassword = "46465"
+accountCity = "Vijayapura"
+
+// console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
