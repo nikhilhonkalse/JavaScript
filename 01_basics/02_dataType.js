7b2e78801b18dfe93a6406a661a2486223bb7baa
Nikhil Honkalse <nikhilhonkalse720@gmail.com>
2025-05-29 16:14:15 +0530

    DataTypes

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -3,4 +3,20 @@
 // alert (3 +3) we are using nodejs, not browser
 
 console.log(3 +
-    3);
+    3); //
+
+let name = "Nikhil"
+let age = 18
+let isLoggedIn = false
+let state;
+
+    // premitive datatypes //
+// number =. 2 to the power 53
+// bigint
+//string => ""
+// Boolean => true/false
+// undefined =>
+//     Symbol => unique
+
+    // Object
+console.log(typeof age);
