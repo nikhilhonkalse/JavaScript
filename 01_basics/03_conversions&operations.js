d081e60b7c8ddebe20281ca0eb4f4039f34d8833
Nikhil Honkalse <nikhilhonkalse720@gmail.com>
2025-05-30 13:40:29 +0530

    others added

--- 01_basics/03_conversions&operations.js
+++ 01_basics/03_conversions&operations.js
@@ -26,5 +26,55 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **********************************Opetaions*************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+// console.log(2/2);
+
+let str1 ="Hello"
+let srt2 =" Nikhil"
+
+let str3 = str1 + srt2
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log("2" + 2 + 2);
+// console.log(2 +2 +"2");
+
+// console.log(3 +3 * 4 % 5); this is the wrong method
+// console.log((3 + 3) * 4 % 5);
+
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+ "true");
+// console.log(+ "");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+let gameCounter1 = 100
+gameCounter++ // postfis increment
+++gameCounter1 //prefix increment
+console.log(gameCounter);
+console.log(gameCounter1);
+
+
+
+
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,59 @@
+// Primitive datatypes
+/*
+// 7 types of datatypes = String, Number, Boolean, null, undefined, Symbol, BigInt
+*********** String *************
+const score = 100
+
+************ Number ************
+const scoreValue  = 100.3
+
+************** Boolean************
+const isLoggedIn = true/false
+
+**************** null**************
+const outSideTemp = null
+
+************** undefined *************
+const userEmail = undefined
+
+************** Symbol************
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+************** BigInt************
+const bigNUmber = 42342337856345789365n
+ console.log(typeof bigNUmber);
+ */
+
+ //Refrence datatypes (Non Premitive)
+
+ // Array, Objects, Functions
+/*
+ const heros = ["Ironman", "Superman", "Batman"]
+
+ let myObj ={
+    name: "Nikhil",
+    age: 25,
+ }
+console.log(myObj);
+
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+
+
+/*
+ Type	              Result
+Undefined	        "undefined"
+Null	            "object" 
+Boolean	            "boolean"
+Number	            "number"
+BigInt	            "bigint"
+String	            "string"
+Symbol	             "symbol"
+Function 	        "function"
+Any other object	"object"
+*/
