fb82b400ddc75ea96751f9fe5d66a568fe659ba8
Nikhil Honkalse <nikhilhonkalse720@gmail.com>
2025-06-07 16:33:14 +0530

    control flow

---
+++ control_flow/one.js
@@ -0,0 +1,53 @@
+// const isUserLoggedIn = true;
+// const tempreature = 40;
+
+// if (tempreature > 40) {
+//     console.log("It's a hot day");
+    
+// }
+// console.log(`tempreature is 50`);
+
+// <, >, <=, >=, ==, ===, !=, !==, ===
+
+// const score = 200;
+
+// if (score > 100) {
+//     const power = "speed"
+//     console.log(`my power is ${power}`);
+    
+// }
+// console.log(`my power is ${power}`); // This will throw an error because 'power' is not defined outside the if block
+
+
+const balance = 1000;
+
+// if (balance > 500) console.log("You can withdraw money"), console.log("You can buy a new phone");// we can execute multiple statements in a single line without using curly braces/scope, but it's not recommended for readability.
+
+
+// if (balance < 500) {
+//     console.log("You can withdraw money");
+
+// } else if (balance < 750) {
+//     console.log("You can buy a new phone");
+    
+// } else if (balance < 900) {
+//     console.log("You can buy a new laptop");
+    
+// } else {
+//     console.log("You can buy a new car");
+    
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+loggedInFromGoogle = false;
+loggedInFromEmail = true;
+
+if (userLoggedIn && debitCard && 2==3) {
+    // console.log("Allow to buy a new phone");
+    
+}
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log(" User has logged in")
+    
+}
